525e74169790271910c1d35d31a5037c14f2c0fa
DonToli <strt87@yandex.ru>
2022-02-08 10:53:46 +0300

    Web application. To practice writing code and improve the application.

---
+++ src/main/java/com/example/twitte/TwitteApplication.java
@@ -0,0 +1,13 @@
+package com.example.twitte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/twitte/TwitteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twitte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
