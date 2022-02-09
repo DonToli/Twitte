9c27e37253f2aa6cc9c43ffa6968ddf64b58fdf6
DonToli <strt87@yandex.ru>
2022-02-09 12:51:07 +0300

    Added new user registration Logging in with login and password Spring Security

--- src/main/java/com/example/twitte/controller/MainController.java
+++ src/main/java/com/example/twitte/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.twitte;
+package com.example.twitte.controller;
 
 import com.example.twitte.domain.Message;
 import com.example.twitte.repos.MessageRepo;
@@ -8,22 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(value = "name", required = false,
-            defaultValue = "World") String name, Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting( Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
 
@@ -31,7 +28,7 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
         messageRepo.save(message);
---
+++ src/main/java/com/example/twitte/domain/Role.java
@@ -0,0 +1,5 @@
+package com.example.twitte.domain;
+
+public enum Role {
+    USER;
+}
