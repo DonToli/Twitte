c7400444d340b319eec49c84efe32d4b99ab36e4
DonToli <strt87@yandex.ru>
2022-04-03 14:39:25 +0300

    Add reCaptcha, rest client, rememberMe и сохранение сессий в БД.

--- src/main/java/com/example/twitte/domain/User.java
+++ src/main/java/com/example/twitte/domain/User.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.util.Collection;
 import java.util.Objects;
@@ -25,10 +26,10 @@ public class User implements UserDetails {
     private String username;
     @NotBlank(message = "Password cannot be empty")
     private String password;
-    @Transient
-    private String password2;
     private boolean active;
 
+    @Email(message = "Email is not correct")
+    @NotBlank(message = "Email cannot be empty")
     private String email;
     private String activationCode;
 
